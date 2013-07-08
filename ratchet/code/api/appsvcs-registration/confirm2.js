8ff9008e603bdba2ec20364b5dc55506f35a1ba8
Michael Uzquiano <uzquiano@gmail.com>
2013-07-08 15:09:49 -0400

    Updated to indicate how to specify password/paymentMethod ahead of time.

---
+++ ratchet/code/api/appsvcs-registration/confirm2.js
@@ -0,0 +1,13 @@
+// the application
+var applicationId = "GUID1";
+
+// the registration hash
+var hash = "HASH";
+
+// confirm the registration (and send welcome email)
+$.ajax({
+    "method": "POST",
+    "type": "json",
+    "processData": false,
+    "url": "/pub/applications/" + applicationId + "/registrations/" + hash + "/confirm"
+});
